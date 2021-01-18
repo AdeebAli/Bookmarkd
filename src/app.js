37ae00af15d113572a570a368bc631768103f759
Adeeb Ali <22379775+AdeebAli@users.noreply.github.com>
2021-01-18 17:30:59 -0600

    add routing and controller structure

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const logger = require('./logger');
 
+const router = require('./router');
+
 const app = express();
 
 // log requests, response status and total req-res time
 app.use(async (req, res, next) => {
-  const startTime = process.hrtime();
+  const startTime = Date.now();
   await next();
   const finishTime = Date.now();
   logger.info(`${res.statusCode} - ${req.method} ${req.url} - ${finishTime - startTime} ms`);
 });
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
-});
+app.use(router);
 
 module.exports = app;
---
+++ src/controllers/base/get/index.js
@@ -0,0 +1,5 @@
+const controller = (req, res) => {
+  res.status(200).send('Bookmarkd API');
+};
+
+module.exports = controller;
